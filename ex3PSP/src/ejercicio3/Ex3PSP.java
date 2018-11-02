0ec28b5ef3cebc9bd41dffc8f453207c2c920d55
andrea <andrea@Drew>
2018-11-02 19:21:28 +0100

    EX3

---
+++ ex3PSP/src/ejercicio3/Ex3PSP.java
@@ -0,0 +1,30 @@
+/**
+ * Realizar un programa que cree un fío, que a súa vez cree outro fío, e así
+ * consecutivamente, ata un total de cinco fíos. Cada fío debe presentar os e u
+ * nome indicando que comenzou, crear os eu fío fillo e presentar os e u nome 10
+ * veces indicando que se está procesando, esperando un tempo aleatorio entre
+ * 100 e 600 milisegundos entre cada presentación. Cada fío deberá esperar a que
+ * o seu fillo termine antes de presentar os eu último mensaxe indicando os eu
+ * nome e que rematou.*
+ */
+package ejercicio3;
+
+/**
+ *
+ * @author oracle
+ */
+public class Ex3PSP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FiosFillos h = null;
+
+        h = new FiosFillos(1); // Se crea el hijo
+        h.start();
+        
+        System.out.println("5 HILOS CREADOS...");
+
+    }
+}
